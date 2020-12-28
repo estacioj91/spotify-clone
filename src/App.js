342183b628fa08e062c39069334b42d2fd8b8f7c
jonathan estacio <estacioj91@yahoo.com>
2020-12-28 14:45:29 -0800

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import Player from "./Player";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [token, setToken] = useState(null);
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = "";
+		const _token = hash.access_token;
+		if (_token) {
+			setToken(_token);
+			spotify.setAccessToken(_token);
+			spotify.getMe().then((user) => {
+				console.log("person", user);
+			});
+		}
+	}, []);
+	return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/Player.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Player = () => {
+	return (
+		<div>
+			<h1>Welcome</h1>
+		</div>
+	);
+};
+export default Player;
