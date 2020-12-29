3e5fe268e447a950ea56f5cf92cfb9a188616aeb
jonathan estacio <estacioj91@yahoo.com>
2020-12-29 15:01:49 -0800

    song list

--- src/Body.js
+++ src/Body.js
@@ -5,11 +5,11 @@ import React from "react";
 import "./Body.css";
 import { useDataLayerValue } from "./DataLayer";
 import Header from "./Header";
+import SongRow from "./SongRow";
 const Body = ({ spotify }) => {
 	const [{ discover_weekly }, dispatch] = useDataLayerValue();
 	return (
 		<div className="body">
-			<h1>Body</h1>
 			<Header spotify={spotify} />
 			<div className="body__info">
 				<img src={discover_weekly?.images[0].url} alt="discover weekly" />
@@ -18,13 +18,16 @@ const Body = ({ spotify }) => {
 					<h2>Discover Weekly</h2>
 					<p>{discover_weekly?.description}</p>
 				</div>
-				<div className="body__songs">
-					<div className="body__icons">
-						<PlayCircleFilledIcon className="body__shuffle" />
-						<FavoriteIcon fontSize="large" />
-						<MoreHorizIcon />
-					</div>
+			</div>
+			<div className="body__songs">
+				<div className="body__icons">
+					<PlayCircleFilledIcon className="body__shuffle" />
+					<FavoriteIcon fontSize="large" />
+					<MoreHorizIcon />
 				</div>
+				{discover_weekly?.tracks.items.map((item) => (
+					<SongRow track={item.track} />
+				))}
 			</div>
 		</div>
 	);
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
 	playlist: [],
 	playing: false,
 	item: null,
-	token:
-		"BQD2o4YSdLDq5waqHzRJC_OkiVU8Nuyb9Ht-oxjSBJd98ynhgX73ZcV5WHYC-elW-Ay1ESXS9LvGp5E7UPCHZKUzEsapHUoOSpy9bEmbyIRRPkeCAyXikn1LKMTpK7t8gqSkuU94BlKiu637LEJZaKSJb9OnwfNsiSpEEV4bI3yaGWR1",
+	// token:
+	// 	"BQD2o4YSdLDq5waqHzRJC_OkiVU8Nuyb9Ht-oxjSBJd98ynhgX73ZcV5WHYC-elW-Ay1ESXS9LvGp5E7UPCHZKUzEsapHUoOSpy9bEmbyIRRPkeCAyXikn1LKMTpK7t8gqSkuU94BlKiu637LEJZaKSJb9OnwfNsiSpEEV4bI3yaGWR1",
 };
 export const reducer = (state, action) => {
 	console.log(action);
